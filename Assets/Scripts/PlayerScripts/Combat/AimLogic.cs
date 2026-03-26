e4a1dd4a472952ea6e7ce5a5c1bf9e5b886d0735
Flavio Telles Paschoal Santos <flaviotps@gmail.com>
2026-03-26 11:30:33 -0300

    Recreate Worms2-style rope movement and PC controls

--- Assets/Scripts/PlayerScripts/Combat/AimLogic.cs
+++ Assets/Scripts/PlayerScripts/Combat/AimLogic.cs
@@ -4,31 +4,74 @@ using System.Collections;
 public class AimLogic : MonoBehaviour {
 
 	private MeshRenderer meshRenderer;
+	private PlayerController playerController;
+	private Rigidbody2D playerRb;
+	private Camera mainCam;
 
+	[Header("PC Controls")]
+	public bool allowMouseAim = true;
+	public float airborneAutoAimMinSpeed = 0.1f;
 
-	
-	// Use this for initialization
 	void Start () 
 	{
 		meshRenderer = gameObject.GetComponent<MeshRenderer>();
+		playerController = GetComponentInParent<PlayerController>();
+		playerRb = GetComponentInParent<Rigidbody2D>();
+		mainCam = Camera.main;
 		meshRenderer.enabled = false;
 	}
 	
-	// Update is called once per frame
 	void Update () 
 	{
-		Vector3 aimVector = new Vector2(Input.GetAxis ("RightHorizontal"), Input.GetAxis ("RightVertical"));
+		bool canManualAim = playerController && playerController.grounded;
+		Vector2 aimVector = Vector2.zero;
 
-		//If player aims, activate crosshair and set rotation
-		if(Input.GetAxis ("RightHorizontal") != 0 || Input.GetAxis ("RightVertical") != 0)
+		if (canManualAim)
 		{
-			transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, Mathf.Atan2(aimVector.x *-1, aimVector.y * -1) * Mathf.Rad2Deg);
+			aimVector = GetManualAimVector();
+		}
+		else if(playerRb && playerRb.velocity.magnitude >= airborneAutoAimMinSpeed)
+		{
+			aimVector = playerRb.velocity.normalized;
+		}
+
+		if(aimVector.sqrMagnitude > 0.0001f)
+		{
+			transform.eulerAngles = new Vector3(
+				transform.eulerAngles.x,
+				transform.eulerAngles.y,
+				Mathf.Atan2(aimVector.x *-1, aimVector.y * -1) * Mathf.Rad2Deg
+			);
 			meshRenderer.enabled = true;
 		}
 		else
-			//Deactivate crosshair when not aiming;
+		{
 			meshRenderer.enabled = false;
+		}
 	}
 
+	Vector2 GetManualAimVector()
+	{
+		Vector2 stickAim = new Vector2(Input.GetAxis ("RightHorizontal"), Input.GetAxis ("RightVertical"));
+		if (stickAim.sqrMagnitude > 0.01f)
+			return stickAim;
 
+		Vector2 keyboardAim = Vector2.zero;
+		if (Input.GetKey(KeyCode.UpArrow)) keyboardAim.y += 1f;
+		if (Input.GetKey(KeyCode.DownArrow)) keyboardAim.y -= 1f;
+		if (Input.GetKey(KeyCode.LeftArrow)) keyboardAim.x -= 1f;
+		if (Input.GetKey(KeyCode.RightArrow)) keyboardAim.x += 1f;
+		if (keyboardAim.sqrMagnitude > 0.01f)
+			return keyboardAim.normalized;
+
+		if (allowMouseAim && mainCam)
+		{
+			Vector3 mouseWorld = mainCam.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 mouseAim = mouseWorld - transform.position;
+			if (mouseAim.sqrMagnitude > 0.01f)
+				return mouseAim.normalized;
+		}
+
+		return Vector2.zero;
+	}
 }
--- Assets/Scripts/PlayerScripts/Combat/WeaponManager.cs
+++ Assets/Scripts/PlayerScripts/Combat/WeaponManager.cs
@@ -9,33 +9,63 @@ public class WeaponManager : MonoBehaviour {
 	public float maxLength = 30;
 	public bool hooked;
 
+	[Header("Worms2-style release")]
+	public float ropeReleaseBoost = 1.15f;
+	public float ropeReleaseTangentialBias = 0.75f;
+
 	[HideInInspector]
 	public GameObject hook;
 	[HideInInspector]
 	public RopeLogic hookScript;
 	[HideInInspector]
 	public Transform crosshair;
+
+	private Rigidbody2D rb;
+	private PlayerController playerController;
 	
 	void Awake()
 	{
 		crosshair = transform.Find ("Crosshair");
+		rb = GetComponent<Rigidbody2D>();
+		playerController = GetComponent<PlayerController>();
 	}
 
-	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown ("Shoot Rope"))
+		if(ShouldShootPressed())
 		{
 			if(hook)
 			{
-				DestroyHook();
+				DestroyHook(true);
 			}
-			else
+			else if(CanShootRope())
+			{
 				SpawnHook ();
+			}
 		}
-		if(Input.GetButtonDown ("Jump") && hook)
+
+		if(ShouldReleasePressed() && hook)
 		{
-			DestroyHook();
+			DestroyHook(true);
 		}
+	}
+
+	bool ShouldShootPressed()
+	{
+		return Input.GetButtonDown("Shoot Rope") || Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.E);
+	}
+
+	bool ShouldReleasePressed()
+	{
+		return Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.Q);
+	}
+
+	bool CanShootRope()
+	{
+		if (!playerController)
+			return true;
+
+		// No chão: mira manual livre. No ar: permite tiros em qualquer direção enquanto em movimento.
+		return playerController.grounded || rb.velocity.sqrMagnitude > 0.2f;
 	}
 
 	void SpawnHook()
@@ -47,9 +77,40 @@ public class WeaponManager : MonoBehaviour {
 
 	public void DestroyHook()
 	{
+		DestroyHook(false);
+	}
+
+	public void DestroyHook(bool applyReleaseBoost)
+	{
+		if (applyReleaseBoost)
+		{
+			ApplyReleaseMomentum();
+		}
+
 		Destroy (hook);
 		gameObject.GetComponent<SpringJoint2D>().enabled = false;
 		hook = null;
 		hookScript = null;
+	}
+
+	void ApplyReleaseMomentum()
+	{
+		if (!rb || !hookScript || hookScript.anchors == null || hookScript.anchors.Count == 0)
+			return;
+
+		Vector2 pivot = hookScript.anchors[hookScript.anchors.Count - 1];
+		Vector2 radiusDir = ((Vector2)transform.position - pivot).normalized;
+		if (radiusDir.sqrMagnitude <= 0.0001f)
+			return;
+
+		Vector2 tangentA = new Vector2(-radiusDir.y, radiusDir.x);
+		Vector2 tangentB = -tangentA;
+		Vector2 currentVelocity = rb.velocity;
+
+		Vector2 bestTangent = Vector2.Dot(currentVelocity, tangentA) >= Vector2.Dot(currentVelocity, tangentB) ? tangentA : tangentB;
+		float speed = Mathf.Max(currentVelocity.magnitude, 0.01f);
+		Vector2 boostedVelocity = (currentVelocity + (bestTangent * speed * ropeReleaseTangentialBias)) * ropeReleaseBoost;
+
+		rb.velocity = boostedVelocity;
 	}
 }
